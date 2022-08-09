a20ce61fa36ba6e687979c41c4fc2a9698178045
unknown <mindc1jun21@iiht.tech>
2022-08-09 23:34:25 +0530

    First Commit

---
+++ src/main/java/com/example/demo/HelloWorldGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldGitApplication.class, args);
+	}
+
+}
